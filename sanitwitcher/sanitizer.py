57c4bf51a527f9fffe5792a3a8dcd593d84c4f60
Ben <ben@wainei.net>
2015-07-27 21:49:20 +0200

    added main script

--- sanitwitcher/sanitizer.py
+++ sanitwitcher/sanitizer.py
@@ -12,7 +12,7 @@ class Sanitizer:
   def __init__(self):
     super().__init__()
     
-    self.rules = [MinimumWordCount(), MinimumMessageLength(), EmoteRatio()]
+    self.rules = [MinimumWordCount(), MinimumMessageLength(), EmoteCount(), EmoteRatio()]
   
   # tells whether a message looks sane based on all sorts of factors
   def is_sane(self, message):
@@ -20,9 +20,31 @@ class Sanitizer:
     # check if any of our rules are broken
     for rule in self.rules:
       if rule.broken(message):
-        logger.debug('[sanitizer] message %s broke the %s rule', message, rule)
+        logger.debug('[sanitizer] message %s broke the %s rule', message.strip(), rule)
         return False
     
     # all good, we have a sane message
     return True
   
+
+if __name__ == "__main__":  
+  logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+  
+  s = Sanitizer()
+  
+  count = 0
+  sane = []
+  dirty = []
+  
+  # get channel name
+  with open(os.path.join(os.path.dirname(__file__), '../tests/destiny.log')) as destiny:
+    for line in destiny:
+      count += 1
+      
+      if s.is_sane(line):
+        sane.append(line)
+      else:
+        dirty.append(line)
+
+  print("total lines {total}, sane {sane}, dirty {dirty} ".format(total=count, sane=len(sane), dirty=len(dirty)))
+  print("quality {quality:.2f}% ".format(quality=100*len(sane)/count))
