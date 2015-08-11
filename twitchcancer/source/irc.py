f3ed3c19a1d00dbababfc341cb995e9a3f8dbea7
Ben <ben@wainei.net>
2015-08-11 11:18:56 +0200

    added name()

--- twitchcancer/source/irc.py
+++ twitchcancer/source/irc.py
@@ -12,43 +12,47 @@ from twitchcancer.source.source import Source
 
 # live channel, messages come in as time goes
 class IRC(Source):
-  
+
   def __init__(self, channel):
     super().__init__()
-    
+
     # irc channel to join
     self.channel = channel
-    
+
     # queue used to hold messages coming from the producer (IRC client) and going into our consumer
     self.messages = queue.Queue()
-    
-    # run network communication in a background thread 
+
+    # run network communication in a background thread
     t = threading.Thread(target=self._client_thread)
     t.daemon = True
     t.start()
-  
+
   # we are iterable
   def __iter__(self):
     return self
-  
+
+  # identify us by our channel name (conflicts cross-servers but whatever)
+  def name(self):
+    return self.channel
+
   # return the first unread message in the queue or blocks waiting for one
   def __next__(self):
     try:
       return self.messages.get()
     except KeyboardInterrupt:
-      raise StopIteration 
-  
+      raise StopIteration
+
   # returns the config to use for our IRC client
   def _get_irc_config(self):
     return CONFIG
-    
+
   # background thread to handle all IRC communication
-  def _client_thread(self):    
+  def _client_thread(self):
     c = IRCClient(self._get_irc_config())
     c.call_on_pubmsg = self._on_pubmsg
     c.join(self.channel)
     c.start()
-  
+
   # add messages we received to the queue
   # called from the client thread
   def _on_pubmsg(self, message):
@@ -58,8 +62,8 @@ class IRC(Source):
 
 if __name__ == "__main__":
   logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
-  
+
   source = IRC('#testkappa')
-  
+
   for m in source:
     print(m)
--- twitchcancer/source/source.py
+++ twitchcancer/source/source.py
@@ -3,9 +3,12 @@
 
 # abstract cancer source
 class Source:
-  
+
   def __init__(self):
     pass
-  
+
   def __iter__(self):
     return self
+
+  def name(self):
+    return id(self)
