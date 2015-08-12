6c8bb7171f05a3890565036715b72875f03bde52
Ben <ben@wainei.net>
2015-08-12 13:59:09 +0200

    added d3.js

--- ng-app/app/scripts/controllers/main.js
+++ ng-app/app/scripts/controllers/main.js
@@ -8,6 +8,27 @@
  * Controller of the main view
  */
 angular.module('twitchCancer')
-  .controller('MainCtrl', function ($scope) {
-    $scope.history = [2, 1, 3];
-  });
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.history = [32, 2, 90];
+
+    /*$http.get('http://localhost:8080/history/forsenlol').success(function(data) {
+      $scope.history = data.history;
+    });*/
+  })
+  .directive('barsChart', function ($parse) {
+     return {
+         restrict: 'E',
+         replace: false,
+         scope: {data: '=chartData'},
+         link: function (scope, element, attrs) {
+           var chart = d3.select(element[0]);
+
+            chart.append("div").attr("class", "chart")
+             .selectAll('div')
+             .data(scope.data).enter().append("div")
+             .transition().ease("elastic")
+             .style("width", function(d) { return d + "%"; })
+             .text(function(d) { return d + "%"; });
+         }
+      };
+   });;
