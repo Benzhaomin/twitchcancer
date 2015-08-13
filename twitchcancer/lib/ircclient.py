45174c0e03fa3af7ed702f2a0b820b200babe162
Ben <ben@wainei.net>
2015-08-13 19:23:50 +0200

    be less verbose or we lose time calling logger.debug for nothing

--- twitchcancer/lib/ircclient.py
+++ twitchcancer/lib/ircclient.py
@@ -8,21 +8,21 @@ import itertools
 logger = logging.getLogger('twitchcancer.logger')
 
 class IRCClient(irc.client.SimpleIRCClient):
-  
+
   def __init__(self, config):
     irc.client.SimpleIRCClient.__init__(self)
-    
+
     self.config = config
     self.channel = None
-    
+
   def join(self, channel):
     # leave the previous channel if any
     if self.channel:
       self.connection.part(self.channel)
-      
+
     # save the new active channel
     self.channel = channel
-    
+
     # connect if necessary (and auto-join on welcome)
     if not self.connection.is_connected():
       try:
@@ -41,18 +41,18 @@ class IRCClient(irc.client.SimpleIRCClient):
     else:
       logger.debug('[client]: welcome but channel %s bot found', self.channel)
       sys.exit(1)
-  
+
   def on_join(self, connection, event):
     logger.debug('[client]: joined %s', self.channel)
 
   def on_pubmsg(self, connection, event):
-    logger.debug('[client]: read %s', event.arguments)
-    
+    #logger.debug('[client]: read %s', event.arguments)
+
     # forward the message to a callback if any
     do_nothing = lambda msg: None
     method = getattr(self, "call_on_pubmsg", do_nothing)
     method(event.arguments[0])
-  
+
   def on_disconnect(self, connection, event):
     sys.exit(0)
 
@@ -61,7 +61,7 @@ def debug_on_msg(msg):
 
 if __name__ == "__main__":
   logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
-  
+
   config = {
     'server': 'irc.ca.us.mibbit.net',
     'port': 6667,
