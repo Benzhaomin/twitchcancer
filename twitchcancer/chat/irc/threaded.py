6bba06ae76f2d4e91e3dc6c5f0e5a9112112ff0a
Ben <ben@wainei.net>
2019-03-06 19:53:47 +0100

    move to Twitch API V5

--- twitchcancer/chat/irc/threaded.py
+++ twitchcancer/chat/irc/threaded.py
@@ -111,15 +111,7 @@ class ThreadedIRCMonitor(Monitor):
 
     # find a server hosting a channel
     def find_server(self, channel):
-        try:
-            result = TwitchApi.chat_properties(channel)
-
-            if result:
-                return random.choice(result['chat_servers'])
-        except KeyError as e:
-            logger.warning("got an empty json response to a chat properties request %s", e)
-
-        return None
+        return random.choice(result['chat_servers'])
 
     # join big channels, leave offline ones
     def autojoin(self):
